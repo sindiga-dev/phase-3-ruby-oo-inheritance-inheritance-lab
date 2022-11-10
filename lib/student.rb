0e5b5f00720324fb76d434f30c609f2c9dd84fce
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-11-10 21:20:20 +0300

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,10 @@
-class Student 
-
+require_relative "./user"
+class Student < User
+attr_accessor :knowledge
+def initialize
+    @knowledge=[]
+end
+def learn lesson
+    @knowledge << lesson
+end
 end
